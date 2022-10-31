d5894c090eeef42a2686e634450202764d11d9a3
SilvaCP <79268743+SilvaCP@users.noreply.github.com>
2022-10-30 23:03:26 -0300

    Adicionei  arquivos via upload!

---
+++ CalculaValorTroco.java
@@ -0,0 +1,21 @@
+package br.com.cursologica;
+
+import java.util.Scanner;
+
+public class CalculaValorTroco {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Digite o valor do produto: ");
+		Double valorProduto = scanner.nextDouble();
+		
+		System.out.print("Digite o valor passado pelo cliente: ");
+		Double valorPassadoPeloCliente = scanner.nextDouble();
+		
+		Double resultado = valorPassadoPeloCliente - valorProduto;
+		
+		System.out.println("Troco: " + resultado);
+		
+		scanner.close();
+	}
+
+}
