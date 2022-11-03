1d1b05921e843bfe7f62bc1df0a564fe8fcd9e7b
carlos <carlos@fedora>
2022-11-02 23:49:47 -0300

    Tipos Numéricos em Java.

---
+++ LogicaPrograma/br/com/fundamentos/TiposNumericos.java
@@ -0,0 +1,17 @@
+package br.com.fundamentos;
+
+public class TiposNumericos {
+	public static void main(String[] args) {
+		Byte numeroByte = 127; // -128 até 127
+		Short numeroShort = 1;	// -32768 até 32767
+		Integer numeroInteger = 2147483647;	// -2147483647 até 2147483647
+		Long numeroLong = 1L;	// -9,223372037x10¹8 até 9,223372037x10¹8
+		
+		Float numeroFloat = 1.0f;
+		Double numeroDouble = 1.0;
+		
+		
+		
+	}
+
+}
