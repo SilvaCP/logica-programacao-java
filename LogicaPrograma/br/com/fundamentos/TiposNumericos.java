2b5570f3b3a8ece95f0aec2130f655ec4f088bc7
carlos <carlos@fedora>
2022-11-03 12:06:11 -0300

    Fiz auteração no código.

--- LogicaPrograma/br/com/fundamentos/TiposNumericos.java
+++ LogicaPrograma/br/com/fundamentos/TiposNumericos.java
@@ -2,13 +2,16 @@ package br.com.fundamentos;
 
 public class TiposNumericos {
 	public static void main(String[] args) {
-		Byte numeroByte = 127; // -128 até 127
-		Short numeroShort = 1;	// -32768 até 32767
-		Integer numeroInteger = 2147483647;	// -2147483647 até 2147483647
-		Long numeroLong = 1L;	// -9,223372037x10¹8 até 9,223372037x10¹8
+		byte numeroByte = 127; // -128 até 127
+		short numeroShort = 1;	// -32768 até 32767
+		int numeroInteger = 2147483647;	// -2147483647 até 2147483647
+		long numeroLong = 1L;	// -9,223372037x10¹8 até 9,223372037x10¹8
 		
-		Float numeroFloat = 1.0f;
-		Double numeroDouble = 1.0;
+		float numeroFloat = 1.0f;
+		double numeroDouble = 1.0;
+		
+		double x = 5.0;
+		
 		
 		
 		
