d9c8f91ee2392993658ad833427d8e676c082b99
Carlos <pereiracarlos1047@gmail.com>
2022-11-09 15:03:37 -0300

    Operadores de atribuição.

---
+++ CursoLogicaCapitulo04/br/com/fundamentos/OperadoresAtribuicao.java
@@ -0,0 +1,17 @@
+package br.com.fundamentos;
+
+public class OperadoresAtribuicao {
+public static void main(String[] args) {
+	Double numero = 7.0;
+//	numero += 4;
+//	System.out.println("numero += 4 é: " + numero);
+//	numero -= 4;
+//	System.out.println("numero -= 4 é: " + numero);
+//	numero *= 4; 
+//	System.out.println("numero *= 4 é: " + numero);
+//	numero /= 4;
+//	System.out.println("numero /= 4 é: " + numero);
+	numero %= 4;
+	System.out.println("numero %= 4 é: " + numero);
+}
+}
