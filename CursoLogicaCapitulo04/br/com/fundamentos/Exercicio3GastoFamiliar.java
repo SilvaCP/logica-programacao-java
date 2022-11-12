326ad766b002baf3db035113fca9c6aaac20fd14
Carlos <pereiracarlos1047@gmail.com>
2022-11-11 21:32:45 -0300

    Fiz alteração no nome da classe.

--- CursoLogicaCapitulo04/br/com/fundamentos/Exercicio3GastoFamiliar.java
+++ CursoLogicaCapitulo04/br/com/fundamentos/Exercicio3GastoFamiliar.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  * 
  * */
 
-public class GastoFamiliar {
+public class Exercicio3GastoFamiliar {
 public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	
