54cc6cc8685c6b7852f5e0a5e3447d8d9e51ecf7
Carlos <pereiracarlos1047@gmail.com>
2022-11-12 01:07:49 -0300

    teste

---
+++ CursoLogicaCapitulo04/br/com/fundamentos/OperadoresIncrementoDecremento.java
@@ -0,0 +1,24 @@
+package br.com.fundamentos;
+
+public class OperadoresIncrementoDecremento {
+public static void main(String[] args) {
+//	Integer numero = 10;
+//	System.out.println("Antes: " + numero);
+//	numero++;
+//	System.out.println("Depois: " + numero);
+//	++numero;
+//	System.out.println("Agora: " + numero);
+	
+//	Integer numero = 10;
+//	Integer numero02 = ++numero;
+//	Integer numero03 = numero++;
+//	System.out.println("Antes: " + numero + " Depois: " + numero02 + " Agora: " + numero03);
+	
+	Integer numero = 10;
+	--numero; // 9
+	numero++; // 10
+	numero--; // 9
+	++numero; // 10
+	System.out.println("Número: " + numero);
+}
+}
